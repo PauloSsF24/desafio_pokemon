bc90c1939a280fbfadbfa0d790672951891da7be
Paulo <pssf23tc@gmail.com>
2026-03-07 01:07:12 -0300

    Ajustes finais na estrutura do backend

--- backend/src/pokemon/pokemon.module.ts
+++ backend/src/pokemon/pokemon.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule], // 👈 aqui
+  controllers: [PokemonController],
   providers: [PokemonService],
-  controllers: [PokemonController]
 })
 export class PokemonModule {}
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { Module } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { PrismaModule } from "../prisma/prisma.module";
 
 @Module({
-  providers: [UsersService]
+  imports: [PrismaModule],
+  providers: [UsersService],
 })
 export class UsersModule {}
