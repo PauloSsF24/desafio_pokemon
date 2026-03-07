d8e7f71397269466097a547aeb400c023686ca25
Paulo <pssf23tc@gmail.com>
2026-03-07 00:56:01 -0300

    Implementando CRUD de pokemon com validacao de propriedade

--- backend/src/pokemon/pokemon.controller.ts
+++ backend/src/pokemon/pokemon.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from "@nestjs/common";
+import { PokemonService } from "./pokemon.service";
 
-@Controller('pokemon')
-export class PokemonController {}
+@Controller("pokemons")
+export class PokemonController {
+  constructor(private pokemonService: PokemonService) {}
+
+  @Get()
+  findAll() {
+    return this.pokemonService.findAll();
+  }
+
+  @Post()
+  create(@Body() body) {
+    return this.pokemonService.create(body, body.userId);
+  }
+
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() body) {
+    return this.pokemonService.update(id, body, body.userId);
+  }
+
+  @Delete(":id")
+  remove(@Param("id") id: string, @Body() body) {
+    return this.pokemonService.delete(id, body.userId);
+  }
+}
--- backend/src/pokemon/pokemon.service.ts
+++ backend/src/pokemon/pokemon.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable()
-export class PokemonService {}
+export class PokemonService {
+  constructor(private prisma: PrismaService) {}
+
+  create(data, userId) {
+    return this.prisma.pokemon.create({
+      data: {
+        ...data,
+        ownerId: userId,
+      },
+    });
+  }
+
+  findAll() {
+    return this.prisma.pokemon.findMany();
+  }
+
+  async update(id: string, data, userId) {
+    const pokemon = await this.prisma.pokemon.findUnique({ where: { id } });
+
+    if (!pokemon || pokemon.ownerId !== userId) {
+      throw new ForbiddenException();
+    }
+
+    return this.prisma.pokemon.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async delete(id: string, userId) {
+    const pokemon = await this.prisma.pokemon.findUnique({ where: { id } });
+
+    if (!pokemon || pokemon.ownerId !== userId) {
+      throw new ForbiddenException();
+    }
+
+    return this.prisma.pokemon.delete({
+      where: { id },
+    });
+  }
+}
