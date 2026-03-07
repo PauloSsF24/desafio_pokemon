4913846b2a4239835409eed65b0fce48ef3ab89b
Paulo <pssf23tc@gmail.com>
2026-03-07 00:54:13 -0300

    Implementando autenticacao de usuario com jwt

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { UsersService } from "../users/users.service";
+import { PrismaModule } from "../prisma/prisma.module";
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: "supersecret",
+      signOptions: { expiresIn: "1d" },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, UsersService],
 })
 export class AuthModule {}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { UsersService } from "../users/users.service";
+import { JwtService } from "@nestjs/jwt";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  async register(data) {
+    const hashed = await bcrypt.hash(data.password, 10);
+
+    const user = await this.usersService.create({
+      ...data,
+      password: hashed,
+    });
+
+    return user;
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.usersService.findByEmail(email);
+
+    if (!user) throw new UnauthorizedException();
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) throw new UnauthorizedException();
+
+    const token = this.jwtService.sign({
+      sub: user.id,
+      email: user.email,
+    });
+
+    return { access_token: token };
+  }
+}
